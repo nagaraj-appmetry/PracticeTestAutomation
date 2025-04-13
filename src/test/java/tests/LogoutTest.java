b8a0b71765c1fb5636782b49d783882e11f0fd0b
Nagaraj-Murugan <nagaraj@appmetry.com>
2025-04-13 23:43:12 +0530

    Initial Login Logout Selenium Test

--- src/test/java/tests/LogoutTest.java
+++ src/test/java/tests/LogoutTest.java
@@ -2,14 +2,23 @@ package tests;
 
 import Base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LogoutTest extends BaseTest {
 
     @Test(dependsOnMethods = {"tests.LoginTest.loginTest"})
     public void logoutTest() {
-        driver.findElement(By.linkText("Log out")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement logoutLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Log out")));
+        logoutLink.click();
+
         boolean isLoggedOut = driver.getPageSource().contains("Test Login Page");
         Assert.assertTrue(isLoggedOut, "Logout Failed!");
     }
